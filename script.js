ef091acda2a8b2c2077e7e037896c9085cd757cd
FuriFuri22 <canesinfranco7@gmail.com>
2022-09-02 15:56:49 -0300

    resolucion del primer trabajo

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const saludar = (nombre)=>console.log(`Hola ${nombre}`)
+
+// Mostrar el resultado por consola
+saludar('Alejandro');
+
+
+
+const multiplicar= (a, b)=>console.log(a*b)
+
+// Mostrar el resultado por consola
+multiplicar(3, 15)
+
+
+
+
+const crearAlumno= (nombre, apellido, edad, materias)=>console.log({nombre, apellido, edad, materias})
+
+// Mostrar el resultado por consola.
+crearAlumno('Leo', 'Messi', '32', ['TLPA','Base de Datos','Inglés'])
+
+
+
+
+const devolverFecha = (d,m,a)=>console.log(`${d}/ ${m}/ ${a}`)
+
+// Mostrar el resultado por consola.
+devolverFecha('10','09','2022');
